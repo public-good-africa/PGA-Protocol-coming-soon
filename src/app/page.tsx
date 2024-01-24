b0eafabc0356dd6fbe58248b583812e676e793ab
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-01-24 11:04:17 +0100

    initial commit:config boilerplates

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import homeImg from '/public/home.jpg';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='font-bold flex justify-center'>Home page</h1>
+    </div>
+  );
+}
