d9ffb785854de31b39aaa48f85b20bd72f90ff85
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-02-07 01:48:15 +0100

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import homeImg from '/public/home.jpg';
+import Button from '../components/Button';
 
 export default function Home() {
   return (
     <div>
       <h1 className='font-bold flex justify-center'>Home page</h1>
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <button className="bg-[#052e16] p-2 border rounded-full text-white">
+            Join Our Waitlist
+        </button>
+    )
+}
+
+export default Button
