e581c87c34ed940ca77daa539f4b5066c63ff503
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-02-07 02:05:10 +0100

    added social  group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import homeImg from '/public/home.jpg';
 import Button from '../components/Button';
 import Logo from '../components/Logo';
+import SocialGroup from '../components/SocialGroup';
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
       <h1 className='font-bold flex justify-center'>Home page</h1>
       <Button />
       <Logo />
+      <SocialGroup />
+      
     </div>
   );
 }
