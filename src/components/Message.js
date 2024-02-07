b657985da316983291ee34b50e8251f4e3d12fb2
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-02-07 23:09:54 +0100

    correct text

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 function Message() {
     return (
         <div className="text-center flex flex-col justify-center items-center my-10 text-7xl font-extralight gap-4">
-            <p>Empower Web3 Builders</p>
+            <p>Empowering Web3 Builders</p>
             <p>and Change-makers through</p>
             <p>Public Good Funding</p>
         </div>
