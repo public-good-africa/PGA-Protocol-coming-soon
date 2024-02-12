2f756053f112b40cf2d644b40d2e0f8512644eb1
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-02-12 17:03:29 +0100

    updated description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Public good africa',
-  description: 'join our waitlist',
+  description: 'Coming soon!',
   icons: {
     icon: '/logo.svg',
     apple: '/logo.svg'
